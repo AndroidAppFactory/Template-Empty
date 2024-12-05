319c850dae1b216666f36731889fe9b57edb6efb
子勰 <code@bihe0832.com>
2024-12-05 19:46:37 +0800

    基础库升级优化

--- App/src/main/java/com/bihe0832/android/app/MainFragment.kt
+++ App/src/main/java/com/bihe0832/android/app/MainFragment.kt
@@ -2,10 +2,10 @@ package com.bihe0832.android.app
 
 import android.provider.Settings
 import android.view.View
-import com.bihe0832.android.common.debug.item.DebugItemData
-import com.bihe0832.android.common.debug.item.DebugTipsData
+import com.bihe0832.android.common.debug.device.DebugDeviceFragment
+import com.bihe0832.android.common.debug.item.getDebugItem
+import com.bihe0832.android.common.debug.item.getTipsItem
 import com.bihe0832.android.common.debug.module.DebugCommonFragment
-import com.bihe0832.android.common.debug.module.DebugDeviceFragment
 import com.bihe0832.android.lib.adapter.CardBaseModule
 import com.bihe0832.android.lib.utils.intent.IntentUtils
 
@@ -13,22 +13,25 @@ open class MainFragment : DebugCommonFragment() {
 
     override fun getDataList(): ArrayList<CardBaseModule> {
         return ArrayList<CardBaseModule>().apply {
-            add(DebugTipsData("APPFactory的通用组件和工具"))
-            add(DebugItemData("查看应用版本及环境", View.OnClickListener { showAPPInfo() }))
-            add(DebugItemData("查看使用情况", View.OnClickListener { showUsedInfo() }))
+            add(getTipsItem("APPFactory的通用组件和工具"))
+            add(getDebugItem("查看应用版本及环境", View.OnClickListener { showAPPInfo() }))
+            add(getDebugItem("查看使用情况", View.OnClickListener { showUsedInfo() }))
             add(getDebugFragmentItemData("查看设备详细信息", DebugDeviceFragment::class.java))
             add(
-                DebugItemData("打开开发者模式") {
-                    IntentUtils.startSettings(context, Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
+                getDebugItem("打开开发者模式") {
+                    IntentUtils.startSettings(
+                        context,
+                        Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS
+                    )
                 },
             )
             add(
-                DebugItemData("打开应用设置") {
+                getDebugItem("打开应用设置") {
                     IntentUtils.startAppDetailSettings(context)
                 },
             )
-            add(DebugItemData("简单测试函数", View.OnClickListener { testFunc() }))
-            add(DebugItemData("通用测试预处理", View.OnClickListener { preTest(it) }))
+            add(getDebugItem("简单测试函数", View.OnClickListener { testFunc() }))
+            add(getDebugItem("通用测试预处理", View.OnClickListener { preTest(it) }))
         }
     }
 
